8775eadd26c4333510693a9dab8f79c6a51c6839
Florian Hübner <fhuebner@posteo.de>
2024-08-15 21:57:01 +0200

    devbox

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/c3aa7b8938b17aebd2deecf7be0636000d62a2b9.tar.gz
-#! nix-shell -p docker openssl
-#! nix-shell -i sh --pure
-# shellcheck shell=sh
+#!/usr/bin/env sh
 
 set -eu
 cd "$(dirname "$(realpath "$0")")/.."
